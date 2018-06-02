e4597498cdc4d2eae3f2fb43a3c74785a5a687cc
papandas <hexoindia@gmail.com>
2018-06-02 11:31:00 +0530

    Test Front-End & Deploy

--- app.js
+++ app.js
@@ -1,16 +1,44 @@
 var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 // Configure App
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
 
 // User middleware
+app.use(bodyParser());
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
 // Define Routes
 
+
+var todoItems = [
+    {id:1, desc: 'foo'},
+    {id:2, desc: 'boo'},
+    {id:3, desc: 'faz'},
+];
+
 app.get('/', function(req, res){
-    res.send('Hello World')
+    res.render('index', {
+        title: 'My App',
+        items: todoItems
+    });
 });
+
+app.post('/add', function (req, res){
+    var newItem = req.body.newItem;
+    console.log(newItem);
+    todoItems.push({
+        id: todoItems.length + 1,
+        desc: newItem
+    });
+
+    res.redirect('/');
+});
+
 
 app.listen(1337, function(){
     console.log('Localhost running on port 1337');
